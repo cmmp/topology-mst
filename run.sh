aea632a42cb6aceda3012d4f7a8788c4c045472c
Cassio M. M. Pereira <cassiomartini@gmail.com>
2014-03-06 17:53:41 -0300

    improving run script

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ fi
 # run analysis:
 echo "Running analysis..."
 #MAVEN_OPTS="-Xmx1024m" mvn exec:java -Dexec.mainClass="br.fapesp.topology.TopologyAnalysis" -Dexec.args="cantor.dat"
-MAVEN_OPTS="-Xmx1024m" mvn -q exec:java -Dexec.mainClass="br.fapesp.topology.TopologyAnalysis" -Dexec.args="cantor.dat" > results.dat
+export MAVEN_OPTS="-Xmx1024m"
+time mvn -q -e exec:java -Dexec.mainClass="br.fapesp.topology.TopologyAnalysis" -Dexec.args="cantor.dat" > results.dat
 
 
